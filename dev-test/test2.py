02b37a6a50519264b91f3f8d80c686beb524f328
zry <admin@z-touhou.org>
2015-12-03 05:14:40 +0800

    ~ Signed-off-by: zry <admin@z-touhou.org>

--- dev-test/test2.py
+++ dev-test/test2.py
@@ -4,9 +4,17 @@ import logging,redis
 print "Ready"
 
 r = redis.Redis(host='10.7.0.1', port=6379, db=2)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logh = BaseRedisHandler(r,'TestApp','TestSource',prefix='log_')
+logc = logging.StreamHandler()
+logh.setLevel(logging.DEBUG)
+logc.setLevel(logging.DEBUG)
+logh.setFormatter(formatter)
+logc.setFormatter(formatter)
 logger = logging.getLogger('main')
 logger.addHandler(logh)
+logger.addHandler(logc)
+logger.setLevel(logging.DEBUG)
 
 logger.info('hello, world~ This is an error~ Nyan~~~~~~')
 

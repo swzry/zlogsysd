77a00def3799ff2ab2c37221ea7348ec5916cc81
zry <admin@z-touhou.org>
2016-01-12 19:10:56 +0800

    Signed-off-by: zry <admin@z-touhou.org>

--- utils/CommonFilter.py
+++ utils/CommonFilter.py
@@ -7,8 +7,10 @@ class CommonFilter():
 	def __init__(self,ModelClass,logger=NoneFunction):
 		self.modelclass = ModelClass
 		self.logger=logger
-		self.filterlist = {}
+		self.filterlist = []
+		self.filterdict = {}
 	def AddFilter(self,name,fieldname,mode,**kwargs):
-		self.filterlist[name] = (fieldname,mode,kwargs)
+		self.filterdict[name] = (fieldname,mode,kwargs)
+		self.filterlist.append((name,(fieldname,mode,kwargs)))
 	def RenderHTML(self):
 		return template('CommonFilter.html',{"fl":self.filterlist,"logger":self.logger})
